be3ac52e2ce4696855f85c5ec9fcb5e4d467efca
sam-fsd <kabirusam037@gmail.com>
2024-03-16 17:47:07 +0300

    simple RESTapi with express

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+const app = express();
+app.use(express.json());
+
+app.get('/api/persons', (req, res) => {
+  res.send(JSON.stringify(persons));
+});
+
+app.get('/info', (req, res) => {
+  const dateTime = new Date().toString();
+  const info = `
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${dateTime}</p>`;
+  res.send(info);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+  if (person) {
+    res.send(JSON.stringify(person));
+  } else {
+    res.statusMessage = 'The person with that id was not found';
+    res.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+
+  res.status(204).end();
+});
+
+app.post('/api/persons', (req, res) => {
+  const id = Math.floor(Math.random() * 100);
+  const body = req.body;
+
+  if (!body.name) {
+    return res.status(400).json({ error: 'Missing the name' });
+  }
+  if (!body.number) {
+    return res.status(400).json({ error: 'Missing the number' });
+  }
+
+  const person = {
+    id,
+    name: body.name,
+    number: body.number,
+  };
+
+  const existingName = persons.find((person) => person.name === body.name);
+  if (existingName) {
+    return res.status(400).json({ error: 'name must be unique' });
+  }
+  persons = persons.concat(person);
+  return res.status(201).json({ success: 'Person created successfuly' });
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log('Server running on port', PORT);
+});
